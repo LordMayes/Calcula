8377b4d9891cec92ecb02af38f23942c766e1909
LordMayes <126431134+LordMayes@users.noreply.github.com>
2023-06-17 11:03:25 -0600

    Agregar archivos de proyecto.

---
+++ Calcula/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calcula
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            pkOper.Items.Add("Sumar");
+            pkOper.Items.Add("Restar");
+            pkOper.Items.Add("Multiplicar");
+            pkOper.Items.Add("Dividir");
+        }
+
+        private void pkOper_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String op = pkOper.SelectedItem.ToString();
+            int n1 = Int32.Parse(txtN1.Text);
+            int n2 = Int32.Parse(txtN2.Text);
+            double res = 0;
+
+            if (op == "Sumar")
+                res = n1 + n2;
+            else if (op == "Restar")
+                res = n1 - n2;
+            else if (op == "Multiplicar")
+                res = n1 * n2;
+            else
+                res = n1 / n2;
+
+
+            lblRes.Text = res + "";
+        }
+    }
+}
